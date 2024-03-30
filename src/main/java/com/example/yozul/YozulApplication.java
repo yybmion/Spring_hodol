d7afb74852e99404d5c774432c8b73dbc22c4199
yoobinmion <yyb400@ajou.ac.kr>
2024-03-30 13:24:07 +0900

    first commit

---
+++ src/main/java/com/example/yozul/YozulApplication.java
@@ -0,0 +1,13 @@
+package com.example.yozul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YozulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YozulApplication.class, args);
+	}
+
+}
